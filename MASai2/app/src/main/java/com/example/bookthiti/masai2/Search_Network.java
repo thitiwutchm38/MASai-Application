725fdf679992a938884ec25cf6c1e38f1682c362
Thitiwut.C <wakawaka2537@gmail.com>
2018-12-03 17:34:06 +0700

    Update:

--- MASai2/app/src/main/java/com/example/bookthiti/masai2/Search_Network.java
+++ MASai2/app/src/main/java/com/example/bookthiti/masai2/Search_Network.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_search__network);
 
 
         }
