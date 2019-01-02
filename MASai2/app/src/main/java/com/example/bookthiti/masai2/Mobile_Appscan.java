f6bdc88084b5b712418d6a354079e8eaafbed3ce
Thitiwut.C <wakawaka2537@gmail.com>
2019-01-03 04:46:00 +0700

    progress bar : mock up

--- MASai2/app/src/main/java/com/example/bookthiti/masai2/Mobile_Appscan.java
+++ MASai2/app/src/main/java/com/example/bookthiti/masai2/Mobile_Appscan.java
@@ -1,35 +1,34 @@
 package com.example.bookthiti.masai2;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
 
-public class Mobile_Appscan extends AppCompatActivity implements SearchView.OnQueryTextListener{
+public class Mobile_Appscan extends AppCompatActivity {
 
     Button button;
-
-
-    // Declare Variables
-    ListView list;
-    ListViewAdapter adapter;
-    SearchView editsearch;
-    String[] animalNameList;
-    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+    ConstraintLayout ct;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile__appscan);
 
+
+
+        ct = (ConstraintLayout) findViewById(R.id.scan_result);
 
         button=(Button)findViewById(R.id.button_rescan);
         button.setOnClickListener(new View.OnClickListener() {
@@ -43,46 +42,9 @@ public class Mobile_Appscan extends AppCompatActivity implements SearchView.OnQu
         });
 
 
-
-
-        // Generate sample data
-
-        animalNameList = new String[]{"Lion", "Tiger", "Dog",
-                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
-                "Cow","Donkey","Monkey"};
-
-        // Locate the ListView in listview_main.xml
-        list = (ListView) findViewById(R.id.listview);
-
-        for (int i = 0; i < animalNameList.length; i++) {
-            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
-            // Binds all strings into an array
-            arraylist.add(animalNames);
-        }
-
-        // Pass results to ListViewAdapter Class
-        adapter = new ListViewAdapter(this, arraylist);
-
-        // Binds the Adapter to the ListView
-        list.setAdapter(adapter);
-
-        // Locate the EditText in listview_main.xml
-        editsearch = (SearchView) findViewById(R.id.SearchView_GooglePlay);
-        editsearch.setOnQueryTextListener(Mobile_Appscan.this);
     }
 
-    @Override
-    public boolean onQueryTextSubmit(String query) {
 
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        String text = newText;
-        adapter.filter(text);
-        return false;
-    }
 
     public void openActivity_Apps_Result() {
 
--- MASai2/app/src/main/java/com/example/bookthiti/masai2/ScanResultActivity.java
+++ MASai2/app/src/main/java/com/example/bookthiti/masai2/ScanResultActivity.java
@@ -1,13 +1,48 @@
 package com.example.bookthiti.masai2;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ScanResultActivity extends AppCompatActivity {
 
+    LinearLayout line_layout;
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
+
+
+
+        line_layout = (LinearLayout) findViewById(R.id.linearLayout_scan_result);
+
+        line_layout.setVisibility(View.GONE);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar_app_scan);
+                progressBar.setVisibility(View.GONE);
+
+                line_layout.setVisibility(View.VISIBLE);
+
+
+
+            }
+        }, 5000); // Millisecond 1000 = 1 sec
     }
 }
