ec6b95b8ade75fac2e7b6119af716b6737210ea1
Thitiwut.C <wakawaka2537@gmail.com>
2019-01-03 17:02:13 +0700

    Update: 3/01/2019

--- MASai2/app/src/main/java/com/example/bookthiti/masai2/ScanResultActivity.java
+++ MASai2/app/src/main/java/com/example/bookthiti/masai2/ScanResultActivity.java
@@ -1,6 +1,14 @@
 package com.example.bookthiti.masai2;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,6 +23,7 @@ import java.util.Collections;
 public class ScanResultActivity extends AppCompatActivity {
 
     LinearLayout line_layout;
+    NotificationCompat.Builder mBuilder;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -29,11 +38,14 @@ public class ScanResultActivity extends AppCompatActivity {
 
         line_layout.setVisibility(View.GONE);
 
+        //mBuilder = new NotificationCompat.Builder(this);
 
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
+
 
                 final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar_app_scan);
                 progressBar.setVisibility(View.GONE);
@@ -45,4 +57,7 @@ public class ScanResultActivity extends AppCompatActivity {
             }
         }, 5000); // Millisecond 1000 = 1 sec
     }
+
+
+
 }
