ad045df78008d97a12e2cce1189fff284b4fc3e9
Thitiwut.C <wakawaka2537@gmail.com>
2019-01-23 00:24:28 +0700

    23/01

--- MASai2/app/src/main/java/com/example/bookthiti/masai2/news_tab.java
+++ MASai2/app/src/main/java/com/example/bookthiti/masai2/news_tab.java
@@ -2,67 +2,92 @@ package com.example.bookthiti.masai2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class news_tab extends Fragment {
-
     RecyclerView mRecyclerView;
-    List<NewsData> mEmailData = new ArrayList<>();
+    ArrayList<OwaspData> mOwaspList = new ArrayList<>();
 
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.news_tab, container, false);
-        mRecyclerView  = rootView.findViewById(R.id.recyclerView_news);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getContext(),
-                LinearLayoutManager.VERTICAL, false);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
-                DividerItemDecoration.VERTICAL));
-
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-
-        NewsData mNews = new NewsData("Sam", "Weekend adventure",
-                "Let's go fishing with John and others. We will do some barbecue and have soo much fun",
-                "10:42am");
-        mEmailData.add(mNews);
-        mNews = new NewsData("Facebook", "James, you have 1 new notification",
-                "A lot has happened on Facebook since",
-                "16:04pm");
-        mEmailData.add(mNews);
-        mNews = new NewsData("Google+", "Top suggested Google+ pages for you",
-                "Top suggested Google+ pages for you",
-                "18:44pm");
-        mEmailData.add(mNews);
-        mNews = new NewsData("Twitter", "Follow T-Mobile, Samsung Mobile U",
-                "James, some people you may know",
-                "20:04pm");
-        mEmailData.add(mNews);
-        mNews = new NewsData("Pinterest Weekly", "Pins you’ll love!",
-                "Have you seen these Pins yet? Pinterest",
-                "09:04am");
-        mEmailData.add(mNews);
-        mNews = new NewsData("Josh", "Going lunch", "Don't forget our lunch at 3PM in Pizza hut",
-                "01:04am");
-        mEmailData.add(mNews);
-
-        NewsAdapter mMailAdapter = new NewsAdapter(getContext(), mEmailData);
-        mRecyclerView.setAdapter(mMailAdapter);
+            View view =  inflater.inflate(R.layout.tips_tab,container,false);
 
 
-        return rootView;
+            mRecyclerView  = view.findViewById(R.id.recyclerView_Owasp);
+
+            LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getContext(),
+                    LinearLayoutManager.VERTICAL, false);
+            mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                    DividerItemDecoration.VERTICAL));
+
+            mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+
+            String m1 = getResources().getString(R.string.M1);
+            String m2 = getResources().getString(R.string.M2);
+            String m3 = getResources().getString(R.string.M3);
+            String m4 = getResources().getString(R.string.M4);
+            String m5 = getResources().getString(R.string.M5);
+            String m6 = getResources().getString(R.string.M6);
+            String m7 = getResources().getString(R.string.M7);
+            String m8 = getResources().getString(R.string.M8);
+            String m9 = getResources().getString(R.string.M9);
+            String m10 = getResources().getString(R.string.M10);
+
+
+            OwaspData owasp = new OwaspData("I1", "Improper Platform Usage", m1);
+
+            mOwaspList.add(owasp);
+
+            owasp = new OwaspData("I2", "Insecure Data Storage",m2);
+            mOwaspList.add(owasp);
+            owasp = new OwaspData("I3", "Insecure Communication",m3);
+            mOwaspList.add(owasp);
+
+            owasp = new OwaspData("I4", "Insecure Authentication",m4);
+            mOwaspList.add(owasp);
+
+            owasp = new OwaspData("I5", "Insufficient Cryptography",m5);
+            mOwaspList.add(owasp);
+
+            owasp = new OwaspData("I6", "Insecure Authorization",m6);
+            mOwaspList.add(owasp);
+
+            owasp = new OwaspData("I7", "Client Code Quality",m7);
+            mOwaspList.add(owasp);
+
+
+            owasp = new OwaspData("I8", "Code Tampering",m8);
+            mOwaspList.add(owasp);
+
+
+            owasp = new OwaspData("I9", "Reverse Engineering",m9);
+            mOwaspList.add(owasp);
+
+
+            owasp = new OwaspData("I10", "Extraneous Functionality",m10);
+            mOwaspList.add(owasp);
+
+
+
+
+            OwaspAdapter mOwaspAdapter = new OwaspAdapter(getContext(), mOwaspList);
+            mRecyclerView.setAdapter(mOwaspAdapter);
+
+
+
+            return view;
+        }
+
     }
-}
